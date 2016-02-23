55d5d48f3870834ca06d0afd18ef56bc31b15f0d
voillot.virgil@gmail.com <voillot.virgil@gmail.com>
2016-02-23 15:45:45 +0100

    final

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,11 +1,39 @@
 var assert = require('assert'),
     config = require('./../lib/config');
+var expect = require("chai").expect;
 
 
+var obj = {
+    args :"",
+    units :"",
+    ip :""
+  };
+
+ var myConfig = config('toto', 'tata');
+  
+  
 describe('Config', function () {
-    it('has 3 props')
+    it('has 3 props',function(){
+		expect(myConfig.args).to.exist;
+		expect(myConfig.units).to.exist;
+		expect(myConfig.ip).to.exist;
+	});
 
-    it('after construction, 2 props are populated')
 
-    it('has correct key types')
+    it('after construction, 2 props are populated',function()
+	{
+		var number_of_null = 0;
+		if(myConfig.args == null)
+			number_of_null++;
+		if(myConfig.units == null)
+			number_of_null++;
+		if(myConfig.ip == null)
+			number_of_null++;
+		expect(number_of_null > 1).to.be.false;
+	});
+
+    it('has correct key types', function(){
+		expect(myConfig.units).to.equal('toto');
+		expect(myConfig.ip).to.equal('tata');
+	})
 });
--- weather/test/test-format.js
+++ weather/test/test-format.js
@@ -1,6 +1,10 @@
 var assert = require('assert'),
     format = require('./../lib/format');
+var expect = require("chai").expect;
+
 
 describe('format', function () {
-    it('is a constructor of the Format object')
+    it('is a constructor of the Format object',function(){
+		expect(format.Format).to.exist;
+	})
 });
